276560f9168f656a99ab14fac1650de112eaa90e
Douglas Anderson <hockeybuggy@gmail.com>
2021-11-20 15:21:42 -0400

    Initial commit.
    This project is gonna be used in conjunction with my "daybooks". I use
    daily notion pages. One thing that I don't have a very good version of
    yet is linking to specific daybooks relative to today's date. The
    intention of this project is to rebuild on a daily cadence and have
    these links up to date.

---
+++ README.md
@@ -0,0 +1,25 @@
+# Daybook redirects
+
+This is a simple static site that creates redirect pages for my "daybooks" (my
+daily task list "system").
+
+## TODO
+
+- [ ] Build system
+  - [ ] Netlify?
+  - [ ] Link to published version in README
+
+
+- [ ] Generate links in index
+
+- [ ] Links
+  - [X] Today
+  - [X] Yesterday
+  - [X] Tomorrow
+  - [ ] Next Monday
+  - [ ] Last Friday
+  - [ ] This weekend
+
+- [ ] Licence
+- [ ] CI
+  - [ ] Linting?
---
+++ index.ts
@@ -0,0 +1,95 @@
+import * as fs from "fs";
+import { addDays, format, subDays } from "date-fns";
+import { Client } from "@notionhq/client";
+
+require("dotenv").config();
+
+// Initializing a client
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN,
+});
+
+async function generateIndexPage(buildDir: string) {
+  const contents = `
+<!DOCTYPE html>
+<head>
+<meta charset="utf-8">
+<title>Daybook redirects</title>
+</head>
+<body>
+  <h1>Daybook Redirects</h1>
+
+  <p>TODO list all of the urls</p>
+</body>
+`;
+  console.log("Generating Index page.");
+  fs.writeFileSync(`${buildDir}/index.html`, contents);
+}
+
+function generateRedirectPage(name: string, targetUrl: string) {
+  return `
+<!DOCTYPE html>
+<meta charset="utf-8">
+<title>Redirecting to ${name}: ${targetUrl}</title>
+<meta http-equiv="refresh" content="0; URL=${targetUrl}">
+<link rel="canonical" href="${targetUrl}">
+`;
+}
+
+async function fetchAndWriteRedirect(
+  buildDir: string,
+  name: string,
+  dayStr: string
+) {
+  console.log(`Generating ${name} page: ${dayStr}`);
+
+  // This works because the pages match the naming scheme of yyyy-MM-dd
+  const dayBookSearchResponse = await notion.search({ query: dayStr });
+
+  // TODO assert that the results are non empty
+  // TODO assert that the results are the correct page type
+  const targetUrl = dayBookSearchResponse.results[0].url;
+
+  const contents = generateRedirectPage(name, targetUrl);
+
+  fs.writeFileSync(`${buildDir}/${name}.html`, contents);
+}
+
+async function generateTodayPage(buildDir: string, now: Date) {
+  const name = "today";
+  const todayStr = format(now, "yyyy-MM-dd");
+  fetchAndWriteRedirect(buildDir, name, todayStr);
+}
+
+async function generateYesterdayPage(buildDir: string, now: Date) {
+  const name = "yesterday";
+  const todayStr = format(subDays(now, 1), "yyyy-MM-dd");
+  fetchAndWriteRedirect(buildDir, name, todayStr);
+}
+
+async function generateTomorrowPage(buildDir: string, now: Date) {
+  const name = "tomorrow";
+  const todayStr = format(addDays(now, 1), "yyyy-MM-dd");
+  fetchAndWriteRedirect(buildDir, name, todayStr);
+}
+
+(async () => {
+  const now = new Date();
+  console.log(`Running at: ${now}`);
+
+  const meResponse = await notion.users.me({});
+  console.log(`User id: ${meResponse.id}`);
+
+  const buildDir = "./build";
+  console.log(`Setting up build directory: ${buildDir}`);
+  if (fs.existsSync(buildDir)) {
+    fs.rmdirSync(buildDir, { recursive: true });
+  }
+  fs.mkdirSync(buildDir);
+
+  await generateIndexPage(buildDir);
+  await generateYesterdayPage(buildDir, now);
+  await generateTodayPage(buildDir, now);
+  await generateTomorrowPage(buildDir, now);
+  console.log("Done");
+})();
